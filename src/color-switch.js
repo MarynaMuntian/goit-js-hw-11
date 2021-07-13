38eabdddf58961351a4ada14d7936470906b00d1
MarinaMuntian <dp060491smv@gmail.com>
2021-07-13 21:09:49 +0300

    update task-1

---
+++ src/color-switch.js
@@ -0,0 +1,29 @@
+const startButton = document.querySelector('[data-action="data-start"]');
+const stopButton = document.querySelector("data-stop");
+const body = document.querySelector('body');
+let timerId = null;
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const backgroundColor = () => {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+const onClickStart = () => {
+    timerId = setInterval(backgroundColor, 1000);
+    stopButton.disabled = false;
+    startButton.disabled = true;
+};
+
+const onClickStop = () => {
+    clearInterval(timerId);
+    stopButton.disabled = true;
+    startButton.disabled = false;
+};
+
+startButton.addEventListener('click', onClickStart);
+stopButton.addEventListener('click', onClickStop);
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './sass/main.scss';
+import './color-switch';
+import './timer';
+import './promisification';
