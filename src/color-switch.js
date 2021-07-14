fd6269d6d77d86eeae5aba18f28d554698f750fd
MarinaMuntian <dp060491smv@gmail.com>
2021-07-14 10:41:09 +0300

    update task-1

--- src/color-switch.js
+++ src/color-switch.js
@@ -1,5 +1,5 @@
-const startButton = document.querySelector('["data-start"]');
-const stopButton = document.querySelector('["data-stop"]');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 let timerId = null;
 stopButton.disabled = true;
