f70118ac52c2c23299c5b91e185812369c71e3c7
MarinaMuntian <dp060491smv@gmail.com>
2021-07-15 12:59:44 +0300

    Update timer.js

--- src/timer.js
+++ src/timer.js
@@ -6,38 +6,25 @@ const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 
 startButton.disabled = true;
+console.log(dateSelector.value);
+const startButtonActive = () => {
+    if (!dateSelector.value) {
+        return
+    }
+    startButton.disabled = false;
+};
+startButtonActive();
 
-function pad(value) {
-    return String(value).padStart(2, '0');
- }
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = pad(Math.floor(ms / day));
-  // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
-
-const timer = {
-    intervalId: null,
-    isActive: false,
-
+class Timer {
+    constructor({ onTick}) {
+        this.intervalId = null;
+        this.isActive = false;
+        this.onTick = onTick;
+    }
     start() {
-        if (this.isActive) {
-            return;
-         }
+        // if (this.isActive) {
+        //     return;
+        //  }
 
         const finishTime = dateSelector.textContent;
 
@@ -45,18 +32,44 @@ const timer = {
         this.intervalId = setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = finishTime - currentTime;
-            const { days, hours, minutes, seconds } = convertMs(deltaTime);
+            const time = this.convertMs(deltaTime);
+            this.onTick(time);
         }, 1000);
-    },
-
+    }
     stop() {
+        const finishTime = dateSelector.textContent;
+        const currentTime = Date.now();
+        const deltaTime = finishTime - currentTime;
         if (deltaTime === 0) {
             clearInterval(this.intervalId);
             this.isActive = false;
         }
         return;
-     },
-};
+    }
+    convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = this.pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = this.pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+    }
+    pad(value) {
+    return String(value).padStart(2, '0');
+ }
+}
+
+const timer = new Timer({onTick: updateTimerField});
 
 function updateTimerField({ days, hours, minutes, seconds }) {
     days.textContent = `${days}:`;
@@ -65,8 +78,8 @@ function updateTimerField({ days, hours, minutes, seconds }) {
     seconds.textContent = `${seconds}`;
  }
 
-startButton.addEventListener('click', timer.start(), timer.stop());
+startButton.addEventListener('click', timer.start.bind(timer), timer.stop.bind(timer));
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
